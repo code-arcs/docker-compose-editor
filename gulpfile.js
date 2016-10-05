3bb79a562b3ebdddda0dfcefc87ba68ca7ed4c1c
Stephan Köninger <github@stekoe.de>
2016-10-05 14:02:59 +0200

    chore(Background): add docker whale to background

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@ process.env.DCE_DEBUG=true;
 
 gulp.task('serve', function (callback) {
     gulp.watch('main.js', ['electron:restart']);
-    gulp.watch(['src/**/*.scss', 'src/**/*.jsx', 'src/**/*.js', 'src/**/*.html', 'src/**/*.css'], ['build:dev']);
+    gulp.watch(['src/**/*.scss', 'src/**/*.jsx', 'src/**/*.js', 'src/**/*.html', 'src/**/*.css', 'src/**/*.png'], ['build:dev']);
 
     runSequence('electron:start', 'build:dev');
 });
@@ -23,7 +23,7 @@ gulp.task('electron:start', () => electron.start());
 gulp.task('electron:reload', () => electron.reload());
 gulp.task('electron:restart', () => electron.restart());
 gulp.task('copy:static', () => {
-    gulp.src('src/**/*.html')
+    gulp.src(['src/**/*.html', 'src/**/*.png'])
         .pipe(gulp.dest('dist'));
 });
 gulp.task('jsx2js', function () {
